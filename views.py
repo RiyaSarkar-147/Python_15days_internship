8fffbe7e59ba452c7c57dadf7de2968c1771f599
RiyaSarkar-147 <85441073+RiyaSarkar-147@users.noreply.github.com>
2021-06-13 14:57:51 +0530

    Task-Registration page

--- views.py
+++ views.py
@@ -1,24 +1,13 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render 
 from django.http import HttpResponse
 
-def homepageview(request):
+def home(request):
     return render(request,'home.html')
 
-def aboutpageview(request):
+def register(request):
+    return render(request,'register.html')
+    
+def about(request):
     return render(request,'about.html')
-
-def contactpageview(request):
-    return render(request,'contact.html')
-
-def myform(request):
-    return render(request,'myform.html')
-
-def process(request):
-    print("Welcome")
-    print(request.method)
-    print(request.POST)
-    a=int(request.POST['txt1'])
-    b=int(request.POST['txt2'])
-    c=a+b
-    print(c)
-    return render (request,'ans.html',{'mya':a,'myb':b,'mysum':c})
+    
