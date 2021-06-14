44476bfdc659da295c5c80ccc73c193ab0bd150b
RiyaSarkar-147 <85441073+RiyaSarkar-147@users.noreply.github.com>
2021-06-14 16:04:35 +0530

    d4

---
+++ Day4_task/even_no_loop.py
@@ -0,0 +1,6 @@
+# To find the number is even or not
+
+
+num = int(input("enter the number?"))  
+if num%2 == 0:  
+    print("Number is even")  
---
+++ Day4_task/pass_keyword.py
@@ -0,0 +1,7 @@
+# Use of pass
+
+for i in [1,2,3,4,5]:   
+    if(i==4):  
+        pass  
+        print("This is pass block",i)  
+    print(i)  
